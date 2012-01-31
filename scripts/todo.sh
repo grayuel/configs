0b44384278b2bacd507656b8a5b5ccb87ffcbc31
samuel gray <samgray@sonic.net>
2012-01-30 21:57:47 -0800

    slightly broken

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
-awk '/\(A\)/ {print "${offset 20}${color #d78787}"$0}' ~/ToDo/todo.txt
-awk '/\(B\)/ {print "${offset 20}${color #f7f7af}"$0}' ~/ToDo/todo.txt
-awk '/\(C\)/ {print "${offset 20}${color #afd787}"$0}' ~/ToDo/todo.txt
-awk '/\(D\)/ {print "${offset 20}${color #87afd7}"$0}' ~/ToDo/todo.txt
-awk '/\(E\)/ {print "${offset 20}${color #d7afd7}"$0}' ~/ToDo/todo.txt
-awk '/^[^\(]/ {print "${offset 20}${color}"$0}' ~/ToDo/todo.txt
+awk '/\(A\)/ {print "${color #d78787}"$0}' ~/ToDo/todo.txt
+awk '/\(B\)/ {print "${color #f7f7af}"$0}' ~/ToDo/todo.txt
+awk '/\(C\)/ {print "${color #afd787}"$0}' ~/ToDo/todo.txt
+awk '/\(D\)/ {print "${color #87afd7}"$0}' ~/ToDo/todo.txt
+awk '/\(E\)/ {print "${color #d7afd7}"$0}' ~/ToDo/todo.txt
+awk '/^[^\(]/ {print "${color}"$0}' ~/ToDo/todo.txt
