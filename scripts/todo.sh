2e4606e00b6324dc0bac24bc03255b1cca61a4d4
samuel gray <samgray@sonic.net>
2012-02-04 14:28:46 -0800

    updates

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,7 +1,7 @@
-#!/bin/bash 
-awk '/\(A\)/ {print "${color #d78787}"$0}' ~/ToDo/todo.txt
-awk '/\(B\)/ {print "${color #f7f7af}"$0}' ~/ToDo/todo.txt
-awk '/\(C\)/ {print "${color #afd787}"$0}' ~/ToDo/todo.txt
-awk '/\(D\)/ {print "${color #87afd7}"$0}' ~/ToDo/todo.txt
-awk '/\(E\)/ {print "${color #d7afd7}"$0}' ~/ToDo/todo.txt
+#!/bin/bash
+awk '/\(A\)/ {print "${color #8c595a}"$0}' ~/ToDo/todo.txt
+awk '/\(B\)/ {print "${color #bdba73}"$0}' ~/ToDo/todo.txt
+awk '/\(C\)/ {print "${color #97af81}"$0}' ~/ToDo/todo.txt
+awk '/\(D\)/ {print "${color #84aaad}"$0}' ~/ToDo/todo.txt
+awk '/\(E\)/ {print "${color #8c79b5}"$0}' ~/ToDo/todo.txt
 awk '/^[^\(]/ {print "${color}"$0}' ~/ToDo/todo.txt
